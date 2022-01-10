3718ae8db08d4b325da985176820c43ee90b07f5
firthunsps@gmail.com <firthunsps@gmail.com>
2022-01-10 14:12:57 +0100

    Flutter sesion dia 09/12/2021,

--- lib/src/pages/http_page.dart
+++ lib/src/pages/http_page.dart
@@ -1,11 +1,35 @@
+import 'package:componentes_2/src/models/Blog.dart';
+import 'package:componentes_2/src/providers/blog_provider.dart';
 import 'package:flutter/material.dart';
 
 
 class HttpPage extends StatelessWidget {
-  const HttpPage({Key? key}) : super(key: key);
-
+   HttpPage({Key? key}) : super(key: key);
+  final BlogProvider http = BlogProvider();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text('Peticiones HTTP')),
+      body: FutureBuilder(
+        future: http.fetchBlogs(),
+        builder:(BuildContext context, AsyncSnapshot<List<Blog>> snapShot) {
+
+          if (snapShot.hasData && snapShot.data != null){
+            List<Blog> blogs = snapShot.data!;
+            return ListView(
+              children: blogs
+                      .map( (Blog b) => ListTile(
+                    title: Text(b.title!),
+                  ))
+                      .toList()
+             );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+
+    );
   }
 }
